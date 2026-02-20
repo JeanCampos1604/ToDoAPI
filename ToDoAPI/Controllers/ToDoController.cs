f5b284b5b03f7dd6f27e4938a521dcfaef01b944
JeanCampos1604 <jeanflaviodecampos1604@gmail.com>
2026-02-20 08:43:40 -0300

    Adicionando delete note.

--- ToDoAPI/Controllers/ToDoController.cs
+++ ToDoAPI/Controllers/ToDoController.cs
@@ -13,16 +13,18 @@ namespace ToDoAPI.Controllers
 
         public ToDoController(ToDoContext context) => _context = context;
 
+        #region MyRegion
         [HttpGet("all", Name = "GetAllNotes")]
         [SwaggerOperation(Summary = "Retorna todas as notas", OperationId = "GetAllNotes")]
         public async Task<IEnumerable<Notes>> Get()
         {
             return await _context.Notes.AsNoTracking().ToListAsync();
         }
+        #endregion
 
         [HttpGet("{id}", Name = "GetNoteById")]
         [SwaggerOperation(Summary = "Retorna nota por id", OperationId = "GetNoteById")]
-        public async Task<ActionResult<Notes>> GetById(Guid id)      
+        public async Task<ActionResult<Notes>> GetById(string id)      
         {
             var note = await _context.Notes.FindAsync(id);
             if (note == null) return NotFound();
@@ -44,5 +46,32 @@ namespace ToDoAPI.Controllers
 
             return CreatedAtRoute("GetNoteById", new { id = note.Id }, note);
         }
+
+        [HttpDelete("{id}", Name = "DeleteNoteById")]
+        [SwaggerOperation(Summary = "Exclui nota por id", OperationId = "DeleteNoteById")]
+        public async Task<ActionResult<Notes>> DeleteById(string id)
+        {
+            _context.Notes.Remove(new Notes { Id = id.ToString() });
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+
+        /*
+        [HttpPost(Name = "UpdateNote")]
+        [SwaggerOperation(Summary = "Atualiza uma nota", OperationId = "UpdateNote")]
+        public async Task<ActionResult<Notes>> Put([FromBody] Notes note, string id)
+        {
+            if (note == null) return BadRequest();
+
+            note.Id = id;
+            note.CreatedAt = DateTime.UtcNow;
+            note.UpdatedAt = DateTime.UtcNow;
+
+            _context.Notes.Add(note);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtRoute("GetNoteById", new { id = note.Id }, note);
+        }
+        */
     }
 }
